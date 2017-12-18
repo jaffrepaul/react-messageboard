2180ca34357cf704859030eec246920de8d5ee92
paulj <jaffrepaul@gmail.com>
2017-12-18 13:32:18 -0500

    created mock post data in state. passing down to PostList

--- src/App.js
+++ src/App.js
@@ -9,6 +9,41 @@ import PostEdit from './PostListEntryDetail';
 const FourOhFour = () => <h1>404 - This Page Does Not Exist</h1>;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [
+        {
+          title: 'title1',
+          message: 'message1',
+          replies: [
+            {
+              user: 'user1',
+              reply: 'reply1'
+            }
+          ],
+          date: '12/18/2017',
+          time: '5:19:44 PM',
+        },
+        {
+          title: 'title2',
+          message: 'message2',
+          replies: [
+            {
+              user: 'user2',
+              reply: 'reply2'
+            },
+            {
+              user: 'user3',
+              reply: 'reply3'
+            },
+          ],
+          date: '12/18/2017',
+          time: '5:19:44 PM',
+        },
+      ]
+    };
+ }
 
   render() {
     return (
@@ -17,7 +52,7 @@ class App extends Component {
           <Route
             exact path='/posts'
             render={() => (
-              <PostList />
+              <PostList posts={this.state.posts}/>
             )}
           />
           <Redirect exact from='/' to='/posts' />
